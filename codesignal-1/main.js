3620e39bd4579e99f3fcdf3623afc7228dfbb999
charlesnnanna <charlesnnanna1667@gmail.com>
2022-09-09 00:00:14 -0700

    Added new challenge

---
+++ codesignal-1/main.js
@@ -0,0 +1,17 @@
+
+function solution(numbers) {
+    const arr = []
+
+    for (let i = 0; i < numbers.length - 2; i++){
+        if ((numbers[i] < numbers[i+1] && numbers[i+1] > numbers[i+2]) || (numbers[i] > numbers[i+1] && numbers[i+1] < numbers[i+2])) {
+            arr.push(1)
+        } else {
+            arr.push(0)
+        }
+    }
+    console.log(arr)
+}
+
+
+const arr = [4, 1, 3, 0, 5]
+solution(arr)
